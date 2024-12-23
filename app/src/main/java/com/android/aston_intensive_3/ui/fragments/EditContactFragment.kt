59e8e3d715899f7003269af18c0405d57f356bc1
Mikhovich Anton <mikhovich.anton@gmail.com>
2024-12-24 00:32:50 +0300

    Final push: -Added as many features as I had time for -Few of them are working differently from the ТЗ(noticed this only in the end of the work) -most of changes are in the EditContactFragmentFragment, ContactsViewModel and MainActivity

--- app/src/main/java/com/android/aston_intensive_3/ui/fragments/EditContactFragment.kt
+++ app/src/main/java/com/android/aston_intensive_3/ui/fragments/EditContactFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import com.android.aston_intensive_3.R
 import com.android.aston_intensive_3.databinding.FragmentEditContactBinding
 import com.android.aston_intensive_3.models.Contact
 import com.android.aston_intensive_3.ui.viewmodel.ContactsViewModel
